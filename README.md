755f203cda831f04c5688c78c0fc577e4aa993af
laszloluque <laszloluque@gmail.com>
2022-02-07 13:15:12 +0100

    README.md file added

---
+++ README.md
@@ -0,0 +1,16 @@
+# Week 2 Lab – Bash & Git
+## Laszlo Pedro Scheers Luque
+
+### Student No: 21144320
+
+### [Week 2 Lab - Git Hub](https://github.com/laszloluque/Week-2-Dev-Ops-Sec-Lab "Week 2 Lab repo")
+___
+
+```bash
+To start the script.sh run:
+
+bash script.sh
+```
+___
+
+![Markdown Logo](https://markdown-here.com/img/icon256.png)
--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir fol_1
 mkdir fol_2
-touch fol_1/1_1.txt, fol_1/1_2.txt, fol_1/1_3.txt
-touch fol_2/2_1.txt, fol_2/2_2.txt, fol_2/2_3.txt
-rm -r fol_1 fol_2
+touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+# rm -r fol_1 fol_2
+chmod 600 fol_1/1_1.txt fol_2/2_1.txt fol_1/1_3.txt fol_2/2_3.txt
+chmod 770 fol_1/1_2.txt fol_2/2_2.txt
